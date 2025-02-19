98a95d496207e8a29329bb9bf4b340127e3ca859
Zuha Rosario <zuhalourenco@gmail.com>
2025-02-19 15:40:00 +0000

    add comments 2

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -4,6 +4,12 @@ namespace MeuPrograma
 {
     public class Program
     {
+        /// <summary>
+        /// Program starts here
+        /// </summary>
+        /// <param name="args">
+        /// Parameter ends
+        /// </param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
