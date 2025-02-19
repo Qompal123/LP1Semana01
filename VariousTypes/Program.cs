0b8fe940e49ca760a906f64115b0de8fad7d23c7
Zuha Rosario <zuhalourenco@gmail.com>
2025-02-19 20:24:40 +0000

    add int value 5

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            int x = 5;
+            
+            Console.WriteLine("Hello LP1!");
+            Console.WriteLine(x);
+
+        }
+    }
+}
