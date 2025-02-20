ca959fc5d92efb6c3dc650040e1ea07197e73579
Zuha Rosario <zuhalourenco@gmail.com>
2025-02-20 20:42:07 +0000

    add char with unicode correct

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -8,11 +8,18 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
-
+            // Add a int with value 5
             int x = 5;
+
+            // Add a char with unicode value
+
+            char y = '\u1F60';
             
+            // Print messages
             Console.WriteLine("Hello LP1!");
             Console.WriteLine(x);
+            Console.WriteLine(y);
+
 
         }
     }
