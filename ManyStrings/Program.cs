586d69387ffe646eef3aa30544dccc3c7e0d2e9b
Zuha Rosario <zuhalourenco@gmail.com>
2025-02-21 02:52:52 +0000

    Add more stings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -7,11 +7,22 @@ namespace ManyStrings
         private static void Main(string[] args)
         {
 
+            //Add a int value
+
+            int x = 10;
+
             // Add strings
 
             string s = "Lourenco \n18 anos \u0CA0";
 
+            string sy = $"Valor de x é {x} e x * 2 é {x + x}";
+
+            string sc = "Valor de x é" + x;
+ 
             Console.WriteLine(s);
+            Console.WriteLine(sc);
+            Console.WriteLine(sy);
+
         }
     }
 }
