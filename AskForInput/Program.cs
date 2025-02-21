d944a13e608b7078c963e765f8149d8c900619ef
Zuha Rosario <zuhalourenco@gmail.com>
2025-02-21 03:17:05 +0000

    Add number input

---
+++ AskForInput/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            //User selects number
+
+            Console.WriteLine("Write a number: ");
+            
+            string usrnumb = Console.ReadLine();
+
+            Console.WriteLine("Write another number: ");
+
+            string usrnumb2 = Console.ReadLine();
+
+
+            //User number display
+
+            Console.WriteLine(usrnumb);
+
+
+
+        }
+    }
+}
